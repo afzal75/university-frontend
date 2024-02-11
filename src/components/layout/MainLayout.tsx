2f6c84760a930280c8c80f002f59a377cf9c3285
afzal75 <afzalbhuiyan16@gmail.com>
2024-02-11 13:01:29 +0600

    sidebar setup successfully

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,7 +1,8 @@
-import { Layout, Menu } from "antd";
+import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../routes/admin.routes";
-const { Header, Content, Footer, Sider } = Layout
+
+import Sidebar from "./Sidebar";
+const { Header, Content } = Layout
 
 
 // const items: MenuProps["items"] = [
@@ -40,30 +41,7 @@ const { Header, Content, Footer, Sider } = Layout
 const MainLayout = () => {
     return (
         <Layout style={{ height: "100vh" }}>
-            <Sider
-                breakpoint="lg"
-                collapsedWidth="0"
-                onBreakpoint={(broken) => {
-                    console.log(broken);
-                }}
-                onCollapse={(collapsed, type) => {
-                    console.log(collapsed, type);
-                }}
-            >
-                <div
-                    style={{
-                        color: "white",
-                        textAlign: "center",
-                        height: "4rem",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center"
-                    }}>
-                    <h1>PH University</h1>
-                </div>
-                <Menu theme="dark" mode="inline"
-                    defaultSelectedKeys={['4']} items={adminSidebarItems} />
-            </Sider>
+            <Sidebar />
             <Layout>
                 <Header style={{ padding: 0, }} />
                 <Content style={{ margin: '24px 16px 0' }}>
@@ -76,9 +54,6 @@ const MainLayout = () => {
                         <Outlet />
                     </div>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>
-                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
-                </Footer>
             </Layout>
         </Layout>
     );
---
+++ src/pages/student/StudentDashboard.tsx
@@ -0,0 +1,10 @@
+
+const StudentDashboard = () => {
+    return (
+        <div>
+            <h1>Student DashBoard</h1>
+        </div>
+    );
+};
+
+export default StudentDashboard;
